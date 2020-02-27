0acfd7fb2392b39d84059c3a52e6b7a8e5a9ccdf
kongxiangyan <kcigaret@outlook.com>
2020-02-27 23:52:52 +0800

    WIP: discard lint-staged

--- husky.config.js
+++ husky.config.js
@@ -4,6 +4,9 @@
 
 module.exports = {
   hooks: {
+    // Problem: seems due to wins permission settings, sometimes lint-staged may cause file lost
+    // Refer to: https://github.com/okonet/lint-staged/issues?utf8=%E2%9C%93&q=permission+denied
+    // Solution: instead of lint-staged, use npm script to do the precommit lint check
     // 'pre-commit': 'lint-staged',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
   }
