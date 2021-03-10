ea36f187c3b44720df1fd89d341809bcb7cf51d8
kongxiangyan <kcigaret@outlook.com>
2021-03-10 23:14:42 +0800

    chore: add csp meta tag in index.html template

--- config/mobius.config.js
+++ config/mobius.config.js
@@ -6,7 +6,8 @@ const CSS_CDN_ORIGIN = 'https://fonts.googleapis.cnpmjs.org/'
 
 const commonTemplate = {
   title: 'Hello, Mobius Project!',
-  whisper: 'The author is looking for a job as a product manager \\n             For a quickest preview of his info, check https://example.com/',
+  'meta-csp': "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; font-src 'self' *.alicdn.com data:; connect-src *;",
+  whisper: 'The owner is looking for a job as a product manager | business manager \\n             For a quickest preview of his info, check https://www.cigaret.world',
   fonts: [
     // `${PUBLIC_PATH}statics/fonts/Workbench[wdth,wght].woff2`,
     // `${PUBLIC_PATH}statics/fonts/Sixtyfour[wdth,wght].woff2`
