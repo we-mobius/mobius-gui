88193c7518b364983d7c7cbcb12de6d6827472bd
kongxiangyan <kcigaret@outlook.com>
2022-01-08 13:16:22 +0800

    chore: support image import in ts file

---
+++ src/@types/image.d.ts
@@ -0,0 +1,20 @@
+declare module '*.png' {
+  const value: string
+  export default value
+}
+declare module '*.jpg' {
+  const value: string
+  export default value
+}
+declare module '*.jpeg' {
+  const value: string
+  export default value
+}
+declare module '*.gif' {
+  const value: string
+  export default value
+}
+declare module '*.webp' {
+  const value: string
+  export default value
+}
