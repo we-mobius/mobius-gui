b46d9a9355c422f96406dcc1b6e3dbdfdc493272
cigaret <cigaret@localhost.localdomain>
2022-02-10 23:19:51 +0800

    feat(element): add more options to button element

--- src/ts/elements/button.ts
+++ src/ts/elements/button.ts
@@ -1,40 +1,60 @@
-import { createElementMaker } from '../helpers/index'
+import { createElementMaker, toClassString } from '../helpers/index'
 
-import type { EventHandler } from '../libs/mobius-utils'
+import type { EventHandler, ClassUnion } from '../libs/mobius-utils'
 import type { ElementOptions } from '../helpers/index'
 
 export type ButtonElementType = 'Button'
 export interface ButtonElementOptions extends ElementOptions {
   styles?: {
+    classes?: ClassUnion
     type?: ButtonElementType
     name?: string
-    label: string
+    label?: string
+    title?: string
+    value?: any
+    content?: any
   }
   actuations?: {
-    clickHandler?: EventHandler<HTMLButtonElement>
+    clickHandler?: EventHandler<HTMLDivElement>
   }
 }
 
 /**
+ * @param styles.classes Classes of button wrapper.
  * @param styles.type Constraint for the role form-group member.
  * @param styles.name Button name.
  * @param styles.label Button label.
+ * @param styles.title Button title.
+ * @param styles.value Button value.
+ * @param styles.content Content of button, default to string 'This is a button'.
  * @param actuations.clickHandler Button click handler.
  */
 export const makeButtonE = createElementMaker<ButtonElementOptions>({
   marks: {},
   styles: {
+    classes: '',
     type: 'Button',
     name: '',
-    label: ''
+    label: '',
+    title: '',
+    value: '',
+    content: 'This is a button'
   },
   actuations: {
     clickHandler: event => event
   },
   configs: {},
-  prepareTemplate: (view) => {
+  prepareTemplate: (view, { styles }) => {
+    const { classes } = styles
+    const classesString = toClassString(classes)
+
     return view`
-      <button name=${'name'} @click=${'clickHandler'}>${'label'}</button>
+      <div
+        role="button" name=${'name'} class="mobius-cursor--pointer ${classesString}"
+        title=${'title'} @click=${'clickHandler'} data-label=${'label'} data-value=${'value'}
+      >
+        ${'content'}
+      </div>
     `
   }
 })
