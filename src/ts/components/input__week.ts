cae9c1390e763b0e9ea86f448f9e22086be80816
cigaret <cigaret@localhost.localdomain>
2022-02-18 23:21:10 +0800

    feat(component): add week picker driver component & week input driver component

---
+++ src/ts/components/input__week.ts
@@ -0,0 +1,147 @@
+import {
+  Data, replayWithLatest, makeGeneralEventHandler, makeGeneralCallback
+} from 'MobiusUtils'
+import { makeDriverFormatComponent, useGUIDriver_ } from '../helpers/index'
+import { makeWeekInputE } from '../elements/input__week'
+
+import type { ClassUnion, EventHandler } from 'MobiusUtils'
+import type { GUIDriverOptions, GUIDriverLevelContexts, GUIDriverSingletonLevelContexts } from '../helpers/index'
+import type { TemplateResult } from '../libs/lit-html'
+import type { WeekInputElementType, WeekInputValue } from '../elements/input__week'
+
+export interface WeekInputDCSingletonLevelContexts extends GUIDriverSingletonLevelContexts {
+  inputs: {
+    styles: {
+      type: WeekInputElementType
+      name: string
+      classes: ClassUnion
+      direction: 'horizontal' | 'vertical'
+      label: string
+      title: string
+      description: string
+      information: string
+      instructions: any[]
+      value: string
+      min: string
+      max: string
+      step: number | 'any'
+    }
+  }
+  _internals: {
+    styles: {
+      type: WeekInputElementType
+      name: string
+      classes: ClassUnion
+      direction: 'horizontal' | 'vertical'
+      label: string
+      title: string
+      description: string
+      information: string
+      instructions: any[]
+      value: string
+      min: string
+      max: string
+      step: number | 'any'
+    }
+    actuations: {
+      inputHandler: EventHandler<HTMLInputElement>
+      changeHandler: EventHandler<HTMLInputElement>
+      valueChangeHandler: (value: WeekInputValue) => void
+    }
+  }
+  outputs: {
+    value: WeekInputValue
+  }
+}
+
+export const makeWeekInputDC =
+makeDriverFormatComponent<GUIDriverOptions, GUIDriverLevelContexts, WeekInputDCSingletonLevelContexts, TemplateResult>({
+  prepareSingletonLevelContexts: (options, driverLevelContexts) => {
+    const typeD = Data.of<WeekInputElementType>('WeekInput')
+    const nameD = Data.of('')
+    const classesD = Data.of<ClassUnion>('')
+    const directionD = Data.of<'horizontal' | 'vertical'>('horizontal')
+    const labelD = Data.of('')
+    const titleD = Data.of('')
+    const descriptionD = Data.of('')
+    const informationD = Data.of('')
+    const instructionsD = Data.of<any[]>([])
+    const valueD = Data.of('1970-W01')
+    const minD = Data.of('')
+    const maxD = Data.of('')
+    const stepD = Data.of<number | 'any'>('any')
+
+    const typeRD = replayWithLatest(1, typeD)
+    const nameRD = replayWithLatest(1, nameD)
+    const classesRD = replayWithLatest(1, classesD)
+    const directionRD = replayWithLatest(1, directionD)
+    const labelRD = replayWithLatest(1, labelD)
+    const titleRD = replayWithLatest(1, titleD)
+    const descriptionRD = replayWithLatest(1, descriptionD)
+    const informationRD = replayWithLatest(1, informationD)
+    const instructionsRD = replayWithLatest(1, instructionsD)
+    const valueRD = replayWithLatest(1, valueD)
+    const minRD = replayWithLatest(1, minD)
+    const maxRD = replayWithLatest(1, maxD)
+    const stepRD = replayWithLatest(1, stepD)
+
+    const [inputHandlerRD] = makeGeneralEventHandler<HTMLInputElement>()
+    const [changeHandlerRD] = makeGeneralEventHandler<HTMLInputElement>()
+
+    const [valueChangeHandlerRD, , inputValueD] = makeGeneralCallback<WeekInputValue>()
+    const inputValueRD = replayWithLatest(1, inputValueD)
+
+    return {
+      inputs: {
+        styles: {
+          type: typeD,
+          name: nameD,
+          classes: classesD,
+          direction: directionD,
+          label: labelD,
+          title: titleD,
+          description: descriptionD,
+          information: informationD,
+          instructions: instructionsD,
+          value: valueD,
+          min: minD,
+          max: maxD,
+          step: stepD
+        }
+      },
+      _internals: {
+        styles: {
+          type: typeRD,
+          name: nameRD,
+          classes: classesRD,
+          direction: directionRD,
+          label: labelRD,
+          title: titleRD,
+          description: descriptionRD,
+          information: informationRD,
+          instructions: instructionsRD,
+          value: valueRD,
+          min: minRD,
+          max: maxRD,
+          step: stepRD
+        },
+        actuations: {
+          inputHandler: inputHandlerRD,
+          changeHandler: changeHandlerRD,
+          valueChangeHandler: valueChangeHandlerRD
+        }
+      },
+      outputs: {
+        value: inputValueRD
+      }
+    }
+  },
+  prepareTemplate: ({ marks, styles, actuations }) => {
+    return makeWeekInputE({ marks, styles, actuations })
+  }
+})
+
+/**
+ * @see {@link makeWeekInputDC}
+ */
+export const useWeekInputDC = useGUIDriver_(makeWeekInputDC)
