7b0e16be5f329fe9e1f58fb2d1e185051d8df689
cigaret <cigaret@localhost.localdomain>
2022-02-18 23:19:16 +0800

    feat(element): add week picker element and week input element

---
+++ src/ts/elements/input__week.ts
@@ -0,0 +1,111 @@
+import { makeUniqueString } from 'MobiusUtils'
+import { createElementMaker } from '../helpers/index'
+import { makeFormItemLayoutE } from './layout__form-item'
+import { makeWeekPickerE } from './week-picker'
+
+import type { ClassUnion, EventHandler } from 'MobiusUtils'
+import type { ElementOptions } from '../helpers/index'
+import type { WeekPickerValue } from './week-picker'
+
+export type WeekInputElementType = 'WeekInput'
+export interface WeekInputElementOptions extends ElementOptions {
+  styles?: {
+    /**
+     * FormGroup member constraint.
+     */
+    type?: WeekInputElementType
+    /**
+     * FormGroup member constraint.
+     */
+    name?: string
+    classes?: ClassUnion
+    direction?: 'horizontal' | 'vertical'
+    label?: string
+    title?: string
+    description?: string
+    information?: string
+    instructions?: any[]
+    value?: string
+    min?: string
+    max?: string
+    step?: number | 'any'
+  }
+  actuations?: {
+    inputHandler?: EventHandler<HTMLInputElement>
+    changeHandler?: EventHandler<HTMLInputElement>
+    valueChangeHandler?: (value: WeekInputValue) => void
+  }
+}
+export type WeekInputValue = WeekPickerValue
+
+export const makeWeekInputE = createElementMaker<WeekInputElementOptions>({
+  marks: {},
+  styles: {
+    type: 'WeekInput',
+    name: '',
+    classes: '',
+    direction: 'horizontal',
+    label: '',
+    title: '',
+    description: '',
+    information: '',
+    instructions: [],
+    value: '1970-W01',
+    min: '',
+    max: '',
+    step: 'any'
+  },
+  actuations: {
+    inputHandler: event => event,
+    changeHandler: event => event,
+    valueChangeHandler: value => value
+  },
+  configs: {},
+  prepareTemplate: (view, { styles, actuations, utils: { html } }) => {
+    const { classes, direction } = styles
+
+    if (direction === 'horizontal') {
+      const { name, label, title, description, information, instructions, value, min, max, step } = styles
+      const { inputHandler, changeHandler, valueChangeHandler } = actuations
+      const labelPart = html`<div class="mobius-padding-x--r-base" title="${title}">${label}</div>`
+      const descriptionPart = html`<div class="mobius-width--fullpct">${description}</div>`
+      const informationPart = html`${information}`
+      const instructionsPart = html`<div class="mobius-width--fullpct">${instructions.toString()}</div>`
+      return makeFormItemLayoutE({
+        styles: {
+          classes: classes,
+          direction: 'horizontal',
+          label: labelPart,
+          description: descriptionPart,
+          input: makeWeekPickerE({
+            styles: { name, title, value, min, max, step },
+            actuations: { inputHandler, changeHandler, valueChangeHandler }
+          }),
+          information: informationPart,
+          instructions: instructionsPart
+        }
+      })
+    } else {
+      const { name, label, title, description, information, instructions, value, min, max, step } = styles
+      const { inputHandler, changeHandler, valueChangeHandler } = actuations
+      const labelPart = html`<div class="" title="${title}">${label}</div>`
+      const descriptionPart = html`<div class="mobius-width--fullpct">${description}</div>`
+      const informationPart = html`${information}`
+      const instructionsPart = html`<div class="mobius-width--fullpct">${instructions.toString()}</div>`
+      return makeFormItemLayoutE({
+        styles: {
+          classes: classes,
+          direction: 'vertical',
+          label: labelPart,
+          description: descriptionPart,
+          input: makeWeekPickerE({
+            styles: { name, title, value, min, max, step },
+            actuations: { inputHandler, changeHandler, valueChangeHandler }
+          }),
+          information: informationPart,
+          instructions: instructionsPart
+        }
+      })
+    }
+  }
+})
---
+++ src/ts/elements/week-picker.ts
@@ -0,0 +1,102 @@
+import { toClassString, makeUniqueString } from 'MobiusUtils'
+import { createElementMaker } from '../helpers/index'
+
+import type { ClassUnion, EventHandler, SynthesizeEvent } from 'MobiusUtils'
+import type { ElementOptions } from '../helpers/index'
+
+export type WeekPickerElementType = 'WeekPicker'
+export interface WeekPickerElementOptions extends ElementOptions {
+  marks?: {
+    id?: string
+  }
+  styles?: {
+    type?: WeekPickerElementType
+    name?: string
+    classes?: ClassUnion
+    label?: string
+    title?: string
+    description?: string
+    /**
+     * Indicate the order of the picker and its label.
+     * Set to `ltr` means the picker is on the left of the label.
+     * Set to `rtl` means the picker is on the right of the label.
+     *
+     * @default 'rtl'
+     */
+    direction?: 'ltr' | 'rtl'
+    value?: string
+    min?: string
+    max?: string
+    step?: number | 'any'
+  }
+  actuations?: {
+    inputHandler?: EventHandler<HTMLInputElement>
+    changeHandler?: EventHandler<HTMLInputElement>
+    valueChangeHandler?: (value: WeekPickerValue) => void
+  }
+}
+export interface WeekPickerValue {
+  name: string
+  label: string
+  value: string
+  valueAsString: string
+  valueAsDate: Date | null
+  valueAsNumber: number
+}
+
+/**
+ * @todo TODO: add more date format to `WeekPickerValue`.
+ */
+export const makeWeekPickerE = createElementMaker<WeekPickerElementOptions>({
+  marks: {
+    id: ''
+  },
+  styles: {
+    type: 'WeekPicker',
+    name: '',
+    classes: '',
+    label: '',
+    title: '',
+    description: '',
+    direction: 'rtl',
+    value: '1970-W01',
+    min: '',
+    max: '',
+    step: 'any'
+  },
+  actuations: {
+    inputHandler: event => event,
+    changeHandler: event => event,
+    valueChangeHandler: value => value
+  },
+  configs: {},
+  prepareTemplate: (view, { marks, styles, actuations, utils }) => {
+    const { id } = marks
+    const { name, label, classes, direction, value, min, max, step } = styles
+
+    const elementId = id !== '' ? id : makeUniqueString('mobius-week-picker')
+    const inputId = `${elementId}__input`
+
+    const { inputHandler, changeHandler, valueChangeHandler } = actuations
+    const changeHandlerDelegator = (event: SynthesizeEvent<HTMLInputElement>): void => {
+      const { value, valueAsDate, valueAsNumber } = event.target
+      changeHandler(event)
+      valueChangeHandler({ name, label, value, valueAsString: value, valueAsDate, valueAsNumber })
+    }
+    const inputHandlerDelegator = (event: SynthesizeEvent<HTMLInputElement>): void => {
+      inputHandler(event)
+    }
+
+    return view`
+      <div id="${elementId}" class="mobius-layout__horizontal ${toClassString(classes)}" title="${'title'}">
+        <label for="${inputId}" style="display: ${direction === 'rtl' ? 'unset' : 'none'};">${'label'}</label>
+        <input
+          id="${inputId}" type="week"
+          name="${name}" value="${value}" min="${min}" max="${max}" step="${step}"
+          @input=${inputHandlerDelegator} @change=${changeHandlerDelegator}
+        >
+        <label for="${inputId}" style="display: ${direction === 'ltr' ? 'unset' : 'none'};">${'label'}</label>
+      </div>
+    `
+  }
+})
