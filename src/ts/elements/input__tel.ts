2163d80b3bee889f47ebabeb1a018b8ded807de3
cigaret <cigaret@localhost.localdomain>
2022-02-19 12:56:02 +0800

    feat(element): add tel inputor element & tel input element

---
+++ src/ts/elements/input__tel.ts
@@ -0,0 +1,111 @@
+import { makeUniqueString } from 'MobiusUtils'
+import { createElementMaker } from '../helpers/index'
+import { makeFormItemLayoutE } from './layout__form-item'
+import { makeTelInputorE } from './tel-inputor'
+
+import type { ClassUnion, EventHandler } from 'MobiusUtils'
+import type { ElementOptions } from '../helpers/index'
+import type { TelInputorValue } from './tel-inputor'
+
+export type TelInputElementType = 'TelInput'
+export interface TelInputElementOptions extends ElementOptions {
+  styles?: {
+    /**
+     * FormGroup member constraint.
+     */
+    type?: TelInputElementType
+    /**
+     * FormGroup member constraint.
+     */
+    name?: string
+    classes?: ClassUnion
+    direction?: 'horizontal' | 'vertical'
+    label?: string
+    title?: string
+    description?: string
+    information?: string
+    instructions?: any[]
+    value?: string
+    minlength?: number
+    maxlength?: number
+    placeholder?: string
+  }
+  actuations?: {
+    inputHandler?: EventHandler<HTMLInputElement>
+    changeHandler?: EventHandler<HTMLInputElement>
+    valueChangeHandler?: (value: TelInputValue) => void
+  }
+}
+export type TelInputValue = TelInputorValue
+
+export const makeTelInputE = createElementMaker<TelInputElementOptions>({
+  marks: {},
+  styles: {
+    type: 'TelInput',
+    name: '',
+    classes: '',
+    direction: 'horizontal',
+    label: '',
+    title: '',
+    description: '',
+    information: '',
+    instructions: [],
+    value: '',
+    minlength: 0,
+    maxlength: 999,
+    placeholder: ''
+  },
+  actuations: {
+    inputHandler: event => event,
+    changeHandler: event => event,
+    valueChangeHandler: value => value
+  },
+  configs: {},
+  prepareTemplate: (view, { styles, actuations, utils: { html } }) => {
+    const { classes, direction } = styles
+
+    if (direction === 'horizontal') {
+      const { name, label, title, description, information, instructions, value, minlength, maxlength, placeholder } = styles
+      const { inputHandler, changeHandler, valueChangeHandler } = actuations
+      const labelPart = html`<div class="mobius-padding-x--r-base" title="${title}">${label}</div>`
+      const descriptionPart = html`<div class="mobius-width--fullpct">${description}</div>`
+      const informationPart = html`${information}`
+      const instructionsPart = html`<div class="mobius-width--fullpct">${instructions.toString()}</div>`
+      return makeFormItemLayoutE({
+        styles: {
+          classes: classes,
+          direction: 'horizontal',
+          label: labelPart,
+          description: descriptionPart,
+          input: makeTelInputorE({
+            styles: { name, title, value, minlength, maxlength, placeholder },
+            actuations: { inputHandler, changeHandler, valueChangeHandler }
+          }),
+          information: informationPart,
+          instructions: instructionsPart
+        }
+      })
+    } else {
+      const { name, label, title, description, information, instructions, value, minlength, maxlength, placeholder } = styles
+      const { inputHandler, changeHandler, valueChangeHandler } = actuations
+      const labelPart = html`<div class="" title="${title}">${label}</div>`
+      const descriptionPart = html`<div class="mobius-width--fullpct">${description}</div>`
+      const informationPart = html`${information}`
+      const instructionsPart = html`<div class="mobius-width--fullpct">${instructions.toString()}</div>`
+      return makeFormItemLayoutE({
+        styles: {
+          classes: classes,
+          direction: 'vertical',
+          label: labelPart,
+          description: descriptionPart,
+          input: makeTelInputorE({
+            styles: { name, title, value, minlength, maxlength, placeholder },
+            actuations: { inputHandler, changeHandler, valueChangeHandler }
+          }),
+          information: informationPart,
+          instructions: instructionsPart
+        }
+      })
+    }
+  }
+})
---
+++ src/ts/elements/tel-inputor.ts
@@ -0,0 +1,100 @@
+import { toClassString, makeUniqueString } from 'MobiusUtils'
+import { createElementMaker } from '../helpers/index'
+
+import type { ClassUnion, EventHandler, SynthesizeEvent } from 'MobiusUtils'
+import type { ElementOptions } from '../helpers/index'
+
+export type TelInputorElementType = 'TelInputor'
+export interface TelInputorElementOptions extends ElementOptions {
+  marks?: {
+    id?: string
+  }
+  styles?: {
+    type?: TelInputorElementType
+    name?: string
+    classes?: ClassUnion
+    label?: string
+    title?: string
+    description?: string
+    /**
+     * Indicate the order of the inputor and its label.
+     * Set to `ltr` means the inputor is on the left of the label.
+     * Set to `rtl` means the inputor is on the right of the label.
+     *
+     * @default 'rtl'
+     */
+    direction?: 'ltr' | 'rtl'
+    value?: string
+    minlength?: number
+    maxlength?: number
+    placeholder?: string
+  }
+  actuations?: {
+    inputHandler?: EventHandler<HTMLInputElement>
+    changeHandler?: EventHandler<HTMLInputElement>
+    valueChangeHandler?: (value: TelInputorValue) => void
+  }
+}
+export interface TelInputorValue {
+  name: string
+  label: string
+  value: string
+  valueAsString: string
+}
+
+/**
+ * @todo TODO: add more date format to `TelInputorValue`.
+ */
+export const makeTelInputorE = createElementMaker<TelInputorElementOptions>({
+  marks: {
+    id: ''
+  },
+  styles: {
+    type: 'TelInputor',
+    name: '',
+    classes: '',
+    label: '',
+    title: '',
+    description: '',
+    direction: 'rtl',
+    value: '',
+    minlength: 0,
+    maxlength: 999,
+    placeholder: ''
+  },
+  actuations: {
+    inputHandler: event => event,
+    changeHandler: event => event,
+    valueChangeHandler: value => value
+  },
+  configs: {},
+  prepareTemplate: (view, { marks, styles, actuations, utils }) => {
+    const { id } = marks
+    const { name, label, classes, direction, value, minlength, maxlength, placeholder } = styles
+
+    const elementId = id !== '' ? id : makeUniqueString('mobius-tel-inputor')
+    const inputId = `${elementId}__input`
+
+    const { inputHandler, changeHandler, valueChangeHandler } = actuations
+    const changeHandlerDelegator = (event: SynthesizeEvent<HTMLInputElement>): void => {
+      changeHandler(event)
+    }
+    const inputHandlerDelegator = (event: SynthesizeEvent<HTMLInputElement>): void => {
+      const { value } = event.target
+      inputHandler(event)
+      valueChangeHandler({ name, label, value, valueAsString: value })
+    }
+
+    return view`
+      <div id="${elementId}" class="mobius-layout__horizontal ${toClassString(classes)}" title="${'title'}">
+        <label for="${inputId}" style="display: ${direction === 'rtl' ? 'unset' : 'none'};">${'label'}</label>
+        <input
+          id="${inputId}" type="tel"
+          name="${name}" value="${value}" minlength="${minlength}" maxlength="${maxlength}" placeholder="${placeholder}"
+          @input=${inputHandlerDelegator} @change=${changeHandlerDelegator}
+        >
+        <label for="${inputId}" style="display: ${direction === 'ltr' ? 'unset' : 'none'};">${'label'}</label>
+      </div>
+    `
+  }
+})
