84f733427a990c566056abda900ebd1770d00d50
kongxiangyan <kcigaret@outlook.com>
2022-04-27 01:50:18 +0800

    feat: update button element & component

--- src/ts/components/button.ts
+++ src/ts/components/button.ts
@@ -3,20 +3,24 @@ import {
   replayWithLatest,
   binaryTweenPipeAtom,
   switchT, combineLatestT,
-  makeGeneralEventHandler
+  makeGeneralEventHandler, makeGeneralCallback
 } from '../libs/mobius-utils'
 import { makeDriverFormatComponent, useGUIDriver_ } from '../helpers/index'
 import { makeButtonE } from '../elements/index'
 
-import type { EventHandler, SynthesizeEvent } from '../libs/mobius-utils'
+import type { ClassUnion, EventHandler, SynthesizeEvent } from '../libs/mobius-utils'
 import type { TemplateResult } from '../libs/lit-html'
 import type { GUIDriverOptions, GUIDriverLevelContexts, GUIDriverSingletonLevelContexts } from '../helpers/index'
-import type { ButtonElementType } from '../elements/button'
+import type { ButtonElementType, ButtonValue } from '../elements/button'
 
 export interface ButtonDCSchema {
   type: ButtonElementType
   name: string
   label: string
+  title: string
+  value: any
+  content: any
+  isDisabled: boolean
 }
 
 export interface ButtonDCSingletonLevelContexts extends GUIDriverSingletonLevelContexts {
@@ -24,34 +28,46 @@ export interface ButtonDCSingletonLevelContexts extends GUIDriverSingletonLevelC
     styles: {
       schema: ButtonDCSchema
       type: ButtonElementType
+      classes: ClassUnion
       name: string
       label: string
+      title: string
+      value: any
+      content: any
+      isDisabled: boolean
     }
   }
   _internals: {
     styles: {
       type: ButtonElementType
+      classes: ClassUnion
       name: string
       label: string
+      title: string
+      value: any
+      content: any
+      isDisabled: boolean
     }
     actuations: {
       clickHandler: EventHandler<HTMLDivElement>
+      valueChangeHandler: (value: ButtonValue) => void
     }
   }
   outputs: {
     click: SynthesizeEvent<HTMLDivElement>
+    value: ButtonValue
     schema: ButtonDCSchema
   }
 }
 
 /**
- * @param inputs.styles.classes Classes of button wrapper.
- * @param inputs.styles.type Constraint for the role form-group member.
- * @param inputs.styles.name Button name.
- * @param inputs.styles.label Button label.
- * @param inputs.styles.title Button title.
- * @param inputs.styles.value Button value.
- * @param inputs.styles.content Content of button, default to string 'This is a button'.
+ * @param inputs.styles.type Type name of Button element. Constraint for the role form-group member.
+ * @param inputs.styles.classes `classes` that will be added to root element of Button element structure.
+ * @param inputs.styles.name Name of Button.
+ * @param inputs.styles.label Label of Button, currently unused.
+ * @param inputs.styles.title Title of Button, when you put your pointer on it, `title` will show just beside your pointer.
+ * @param inputs.styles.value This value will be emitted when button be clicked instead of default ClickEvent.
+ * @param inputs.styles.content Content of Button, default to string 'Just a button'.
  */
 export const makeButtonDC =
 makeDriverFormatComponent<GUIDriverOptions, GUIDriverLevelContexts, ButtonDCSingletonLevelContexts, TemplateResult>({
@@ -62,22 +78,39 @@ makeDriverFormatComponent<GUIDriverOptions, GUIDriverLevelContexts, ButtonDCSing
 
     // TODO: 将 schemaIn 映射到 styles
     // 组件 styles
-    const nameD = Data.of('')
+    const classesD = Data.of<ClassUnion>('')
     const typeD = Data.of<ButtonElementType>('Button')
+    const nameD = Data.of('')
     const labelD = Data.of('')
-    const nameRD = replayWithLatest(1, nameD)
+    const titleD = Data.of('')
+    const valueD = Data.of<any>('')
+    const contentD = Data.of<any>('Just a button')
+    const isDisabledD = Data.of(false)
+
+    const classesRD = replayWithLatest(1, classesD)
     const typeRD = replayWithLatest(1, typeD)
+    const nameRD = replayWithLatest(1, nameD)
     const labelRD = replayWithLatest(1, labelD)
+    const titleRD = replayWithLatest(1, titleD)
+    const valueRD = replayWithLatest(1, valueD)
+    const contentRD = replayWithLatest(1, contentD)
+    const isDisabledRD = replayWithLatest(1, isDisabledD)
 
     // 组件核心逻辑
     const [clickHandlerRD, , clickD] = makeGeneralEventHandler<HTMLDivElement>()
     const clickRD = replayWithLatest(1, clickD)
+    const [valueChangeHandlerRD, , buttonValueD] = makeGeneralCallback<ButtonValue>()
+    const buttonValueRD = replayWithLatest(1, buttonValueD)
 
     // 表单项约束
     const stylesRD = replayWithLatest(1, combineLatestT({
-      name: nameRD,
       type: typeRD,
-      label: labelRD
+      name: nameRD,
+      label: labelRD,
+      title: titleRD,
+      value: valueRD,
+      content: contentRD,
+      isDisabled: isDisabledRD
     }))
     // report button component styles for every happened click event
     binaryTweenPipeAtom(switchT(stylesRD, clickD), schemaOutRD)
@@ -86,23 +119,35 @@ makeDriverFormatComponent<GUIDriverOptions, GUIDriverLevelContexts, ButtonDCSing
       inputs: {
         styles: {
           schema: schemaInD,
-          name: nameD,
+          classes: classesD,
           type: typeD,
-          label: labelD
+          name: nameD,
+          label: labelD,
+          title: titleD,
+          value: valueD,
+          content: contentD,
+          isDisabled: isDisabledD
         }
       },
       _internals: {
         styles: {
-          name: nameRD,
+          classes: classesRD,
           type: typeRD,
-          label: labelRD
+          name: nameRD,
+          label: labelRD,
+          title: titleRD,
+          value: valueRD,
+          content: contentRD,
+          isDisabled: isDisabledRD
         },
         actuations: {
-          clickHandler: clickHandlerRD
+          clickHandler: clickHandlerRD,
+          valueChangeHandler: valueChangeHandlerRD
         }
       },
       outputs: {
         click: clickRD,
+        value: buttonValueRD,
         schema: schemaOutRD
       }
     }
--- src/ts/elements/button.ts
+++ src/ts/elements/button.ts
@@ -1,59 +1,132 @@
+import { debounceS } from 'MobiusUtils'
 import { createElementMaker, toClassString } from '../helpers/index'
 
-import type { EventHandler, ClassUnion } from '../libs/mobius-utils'
+import type { EventHandler, ClassUnion, SynthesizeEvent } from 'MobiusUtils'
 import type { ElementOptions } from '../helpers/index'
 
 export type ButtonElementType = 'Button'
 export interface ButtonElementOptions extends ElementOptions {
   styles?: {
-    classes?: ClassUnion
+    /**
+     * Type name of Button element. Constraint for the role form-group member.
+     */
     type?: ButtonElementType
+    /**
+     * `classes` that will be added to root element of Button element structure.
+     */
+    classes?: ClassUnion
+    /**
+     * Name of Button.
+     */
     name?: string
+    /**
+     * Label of Button, currently unused.
+     */
     label?: string
+    /**
+     * Title of Button, when you put your pointer on it, `title` will show just beside your pointer.
+     */
     title?: string
+    /**
+     * This value will be emitted when button be clicked instead of default ClickEvent.
+     */
     value?: any
+    /**
+     * Content of Button, default to string 'Just a button'.
+     * @default 'Just a button'
+     */
     content?: any
+    /**
+     * Indicate if button is disabled.
+     * @default false
+     */
+    isDisabled?: boolean
   }
   actuations?: {
+    /**
+     * Even if the button is disabled, it can still be clicked, and `clickHandler` will always be called.
+     */
     clickHandler?: EventHandler<HTMLDivElement>
+    /**
+     * When button is disabled, i.e. `styles.isDisabled` is true, this handler will not be called.
+     *
+     * If you want to monitor click action at any time, regardless of the disabled status of the button,
+     * you can use `clickHandler` instead.
+     */
+    valueChangeHandler?: (value: ButtonValue) => void
   }
+}
+export interface ButtonValue {
+  name: string
+  label: string
+  value: any
+  clickedTimes: number
+  deltaClickedTimes: number
 }
 
 /**
- * @param styles.classes Classes of button wrapper.
- * @param styles.type Constraint for the role form-group member.
- * @param styles.name Button name.
- * @param styles.label Button label.
- * @param styles.title Button title.
- * @param styles.value Button value.
- * @param styles.content Content of button, default to string 'This is a button'.
+ * @param styles.type Type name of Button element. Constraint for the role form-group member.
+ * @param styles.classes `classes` that will be added to root element of Button element structure.
+ * @param styles.name Name of Button.
+ * @param styles.label Label of Button, currently unused.
+ * @param styles.title Title of Button, when you put your pointer on it, `title` will show just beside your pointer.
+ * @param styles.value This value will be emitted when button be clicked instead of default ClickEvent.
+ * @param styles.content Content of Button, default to string 'Just a button'.
  * @param actuations.clickHandler Button click handler.
+ * @param actuations.valueChangeHandler Button value change handler.
  */
 export const makeButtonE = createElementMaker<ButtonElementOptions>({
   marks: {},
   styles: {
-    classes: '',
     type: 'Button',
+    classes: '',
     name: '',
     label: '',
     title: '',
     value: '',
-    content: 'This is a button'
+    content: 'Just a button',
+    isDisabled: false
   },
   actuations: {
-    clickHandler: event => event
+    clickHandler: event => event,
+    valueChangeHandler: value => value
   },
   configs: {},
-  prepareTemplate: (view, { styles }) => {
-    const { classes } = styles
+  prepareTemplate: (view, { styles, actuations }) => {
+    const { classes, name, label, title, value, content, isDisabled } = styles
     const classesString = toClassString(classes)
+
+    const { clickHandler, valueChangeHandler } = actuations
+    const _buttonStates = {
+      clickedTimes: 0,
+      lastClickedTimes: 0
+    }
+    const getDeltaClickedTimes = (): number => {
+      const delta = _buttonStates.clickedTimes - _buttonStates.lastClickedTimes
+      _buttonStates.lastClickedTimes = _buttonStates.clickedTimes
+      return delta
+    }
+    // valueChangeHandler 最终被实际执行的时候再调用 `getDeltaClickedTimes` 以获取正确的点击次数
+    const debouncedValueChangeHandler = debounceS(
+      (value: ButtonValue) => valueChangeHandler({ ...value, deltaClickedTimes: getDeltaClickedTimes() }),
+      200
+    ) as ((value: Omit<ButtonValue, 'deltaClickedTimes'>) => void)
+    const clickHandlerDelegator = (event: SynthesizeEvent<HTMLDivElement>): void => {
+      // 将点击事件第一时间传递给正常的点击事件监听逻辑中
+      clickHandler(event)
+      // 然后再执行自定义逻辑
+      if (!isDisabled) {
+        _buttonStates.clickedTimes += 1
+        debouncedValueChangeHandler({ name, label, value, clickedTimes: _buttonStates.clickedTimes })
+      }
+    }
 
     return view`
       <div
-        role="button" name=${'name'} class="mobius-cursor--pointer ${classesString}"
-        title=${'title'} @click=${'clickHandler'} data-label=${'label'} data-value=${'value'}
+        role="button" name=${'name'} class="mobius-cursor--pointer ${isDisabled ? 'mobius-button--disabled' : 'mobius-button'} ${classesString}"
+        title=${title} @click=${clickHandlerDelegator} data-label=${label} data-value=${'value'} data-is-disabled=${isDisabled}
       >
-        ${'content'}
+        ${content}
       </div>
     `
   }
